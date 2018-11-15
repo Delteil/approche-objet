41525e0221737111da51dcc79000d84350d32110
Amandine <a.delteil83@gmail.com>
2018-11-15 11:07:49 +0100

    TP2 utilisation du constructeur Adressepostal dans le fichier testPersonne

--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -1,5 +1,6 @@
 package essais;
 
+import entites.AdressePostal;
 import entites.Personne;
 
 public class TestPersonne {
@@ -8,39 +9,33 @@ public class TestPersonne {
 		// TODO Auto-generated method stub
 
 		Personne personne1 = new Personne("Delteil", "Amandine");
+		personne1.adresse = new AdressePostal(8, "lotissement Cazanove", 34150, "Montpeyroux");
 
 		/*
-		 * personne1.nom = "Delteil"; 
-		 * personne1.prenom = "Amandine";
+		 * personne1.nom = "Delteil"; personne1.prenom = "Amandine";
 		 */
-		personne1.adresse.codePostal = 34150;
-		personne1.adresse.nomRue = "lotissement Cazanove";
-		personne1.adresse.numeroRue = 8;
-		personne1.adresse.ville = "Montpeyroux";
+		/*
+		 * personne1.adresse.codePostal = 34150; personne1.adresse.nomRue =
+		 * "lotissement Cazanove"; personne1.adresse.numeroRue = 8;
+		 * personne1.adresse.ville = "Montpeyroux";
+		 */
 
-		Personne personne2 = new Personne();
+		Personne personne2 = new Personne("Tasselli", "Laurence");
 
 		/*
-		 * personne2.nom = "Tasselli"; 
-		 * personne2.prenom = "Laurence";
+		 * personne2.nom = "Tasselli"; personne2.prenom = "Laurence";
 		 */
-		personne2.adresse.codePostal = 34150;
-		personne2.adresse.nomRue = "chemin des plantades";
-		personne2.adresse.numeroRue = 334;
-		personne2.adresse.ville = "Saint Jean de Fos";
+		/*
+		 * personne2.adresse.codePostal = 34150; personne2.adresse.nomRue =
+		 * "chemin des plantades"; personne2.adresse.numeroRue = 334;
+		 * personne2.adresse.ville = "Saint Jean de Fos";
+		 */
 
 		personne1.affichePersonne();
 
 		System.out.println();
 
 		personne2.affichePersonne();
-
-		/*
-		 * Conclusion Que pensez-vous du nombre de lignes de code nécessaires pour
-		 * instancier (i.e. créer) une personne ? il est possible de réduire le nombre
-		 * de ligne en créant un constructeur dans le fichier personne. ainsi l'on
-		 * utilise qu'une seule ligne pour chaque nouvelle personne instanciée.
-		 */
 
 	}
 
